38dfad23d124204adb44deab99344532f5d08b17
nyxSeal <155689615+nyxSeal@users.noreply.github.com>
2026-03-15 18:42:34 -0400

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 
+set -e
+
 lsblk
 
 echo -n "Hostname: " 
 read -r host
 
+cd
 
 echo "Partioning disk..."
-nix run github:nix-community/disko -- --mode disko ./../hosts/$host/disko-config.nix
+nix run github:nix-community/disko --extra-experimental-features "nix-command flakes" -- --mode disko ~/nixconfig/hosts/$host/disko-config.nix
 
 echo "Installing NixOS..."
-nixos-rebuild switch --flake ./../#$host
+nixos-install --flake ~/nixconfig#$host
 
 echo "Installation complete!"
