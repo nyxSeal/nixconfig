09236f91361f40d145e6ee89a63351c59b50ae6c
nyxSeal <155689615+nyxSeal@users.noreply.github.com>
2026-03-15 22:42:13 -0400

    update install.sh: add option to generate hardware-configuration.nix

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,3 +26,16 @@ echo "Installing NixOS..."
 nixos-install --flake github:nyxSeal/nixconfig#$host
 
 git clone https://github.com/nyxSeal/nixconfig --branch untested /mnt/home/nyxSeal/.nixconfig
+
+echo -n "Generate hardware-configuration.nix into config (enter 'true' for true)? "
+read -r hardwareConfigurationGeneration
+
+if [[ "$hardwareConfigurationGeneration" == 'true' ]]; then
+
+nixos-generate-config --no-filesystems --dir /mnt/home/$user/.nixconfig/hosts/$host
+rm /mnt/home/$user/.nixconfig/hosts/$host/configuration.nix
+
+fi
+
+nixos-enter --root /mnt -c 'passwd ${username}'
+
