78dd9084b211b56dedf784b224d101224c8d317d
nyxSeal <155689615+nyxSeal@users.noreply.github.com>
2026-03-15 22:46:41 -0400

    update install.sh: move prompt to start of script; move hardware-configuration generation to before installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,10 +15,20 @@ read -r username
 echo -n "Branch of this repo to use: "
 read -r branch
 
+echo -n "Generate hardware-configuration.nix into config (enter 'true' for true)? "
+read -r hardwareConfigurationGeneration
+
 cd
 
 echo "Partioning disk..."
 nix run github:nix-community/disko --extra-experimental-features "nix-command flakes" -- --mode disko ~/nixconfig/hosts/$host/disko-config.nix
+
+
+
+if [[ "$hardwareConfigurationGeneration" == 'true' ]]; then
+  nixos-generate-config --no-filesystems --dir /mnt/home/$user/.nixconfig/hosts/$host
+  rm /mnt/home/$user/.nixconfig/hosts/$host/configuration.nix
+fi
 
 
 
@@ -27,15 +37,6 @@ nixos-install --flake github:nyxSeal/nixconfig#$host
 
 git clone https://github.com/nyxSeal/nixconfig --branch untested /mnt/home/nyxSeal/.nixconfig
 
-echo -n "Generate hardware-configuration.nix into config (enter 'true' for true)? "
-read -r hardwareConfigurationGeneration
-
-if [[ "$hardwareConfigurationGeneration" == 'true' ]]; then
-
-nixos-generate-config --no-filesystems --dir /mnt/home/$user/.nixconfig/hosts/$host
-rm /mnt/home/$user/.nixconfig/hosts/$host/configuration.nix
-
-fi
 
 nixos-enter --root /mnt -c 'passwd ${username}'
 
