8a09277be04b4046013026eafede4b686a9cd53e
nyxSeal <litigate_putdown.zigzagged008@slmails.com>
2026-03-17 19:14:30 -0400

    add more audio options

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -7,7 +7,18 @@
   options.sound-driver.enable = lib.mkEnableOption "enables pipewire/sound";
 
   config = lib.mkIf config.sound-driver.enable {
-    services.pipewire.enable = true;
-    services.pipewire.pulse.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;  
+    };
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+ 
+    environment.systemPackages = with pkgs; [
+      pavucontrol
+    ];
   };
 }
