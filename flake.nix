6c2a321de1f2ea3cc53e3033f226ed50db6d3a19
nyxSeal <155689615+nyxSeal@users.noreply.github.com>
2026-03-18 11:15:17 -0400

    flake.nix: add noctalia inputs

--- flake.nix
+++ flake.nix
@@ -24,6 +24,17 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.noctalia-qs.follows = "noctalia-qs";
+    };
+
+    noctalia-qs = {
+      url = "github:noctalia-dev/noctalia-qs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
