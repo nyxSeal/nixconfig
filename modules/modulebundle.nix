fd3f4896ef837b185cacf5b2670676a543ca22ba
nyxSeal <155689615+nyxSeal@users.noreply.github.com>
2026-03-22 18:06:45 +0000

    modulebundle.nix: move contents of globalvars.nix

--- modules/modulebundle.nix
+++ modules/modulebundle.nix
@@ -29,6 +29,39 @@
     ./system/system-services.nix
     ./system/zsh.nix
   ];
+  options = {
+    mainUser = lib.mkOption {
+      type = lib.types.str;
+      description = "the primary user of the current host";
+    };
+
+    hostName = lib.mkOption {
+      type = lib.types.str;
+      description = "the name of the host";
+    };
+
+    gitUsername = lib.mkOption {
+      type = lib.types.str;
+      default = "nyxSeal";
+      description = "the username of the git user";
+    };
+
+    gitEmail = lib.mkOption {
+      type = lib.types.str;
+      default = "litigate_putdown.zigzagged008@slmails.com";
+      description = "the email of the git user";
+    };
+
+    allowedSshUser = lib.mkOption {
+      type = lib.types.str;
+      description = "the user allowed to SSH into the host";
+    };
+
+    diskName = lib.mkOption {
+      type = lib.types.str;
+      description = "the name of the disk partition (excluding /dev)";
+    };
+  };
 
   config = {
     # DEFAULTS
