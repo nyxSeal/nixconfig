1824cab45b90ab03cc70071e1fb84c2dc848fe18
nyxSeal <155689615+nyxSeal@users.noreply.github.com>
2026-03-22 17:55:33 +0000

    entertainment.nix: remove home manager enable; formatting

--- modules/entertainment/entertainment.nix
+++ modules/entertainment/entertainment.nix
@@ -5,16 +5,13 @@
   inputs,
   ...
 }: {
-  options = {
-    entertainment.enable = lib.mkEnableOption "entertainment, such as games and communication";
-  };
+  options.entertainment.enable = lib.mkEnableOption "entertainment, such as games and communication";
 
   config = lib.mkIf config.entertainment.enable {
-    home-manager.enable = lib.mkOverride 500 true;
 
     programs.steam = {
       enable = true;
-      #remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      #remotePlay.openFirewall = true; # open ports in the firewall for Steam Remote Play
       #dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
       #localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     };
@@ -22,7 +19,7 @@
     users.users."${config.mainUser}".packages = [
       pkgs.prismlauncher # minecraft launcher
       pkgs.vesktop # discord client
-      pkgs.discover-overlay # may allow discord voice to be overlayed in-game
+      pkgs.discover-overlay # may allow discord voice to be overlayed in-game? (untested)
     ];
   };
 }
